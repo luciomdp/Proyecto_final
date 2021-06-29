d7b1540bf6bf07d025be473e11988544c28daf5a
Valen <gc@DESKTOP-BNEAP4S>
2021-06-29 14:21:15 -0300

    todavia me falta solucionar el tema de los tipos de datos. ya hice que lea el archivo de jugadores y un bucle q vaya creando un arraylist de jugadores x equipo a la vez q lee el archivo equipo. no lo revise, porque tengo que solucionar lo que dije en el primer parrafo. a la noche sigo

--- src/backend/Dt.java
+++ src/backend/Dt.java
@@ -5,17 +5,16 @@ import java.util.Date;
 //hacer enum de nacionalidad
 
 public class Dt extends Persona{
-	public Dt(String a, String no, Date n, int dni) {
-		super(a, no, n, dni);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = -3580960837841978040L;
 	private String nacionalidad;
 	private byte titulos;
+	
+	public Dt(String a, String no, Date n, int dni, String na, byte ti) {
+		super(a, no, n, dni);
+		nacionalidad = na;
+		titulos = ti;
+	}
 	
 	public String DatosIdent() {
 		
--- src/backend/Main.java
+++ src/backend/Main.java
@@ -10,38 +10,58 @@ import java.util.stream.Stream;
 public class Main {
 	public static void main (String[] args) {
 		
-	
-		
+		//leo el archivo jugadores y creo un arraylist con tooodos los jugadores
 		String filePath1 = "Jugadores.txt";
-		List <Jugador> jugadores = new ArrayList<>();
+		List <Jugador> totJugadores = new ArrayList<>();
 		try (Stream <String> streamfile = Files.lines(Paths.get(filePath1))){
-			jugadores = streamfile.map(linea -> linea.split (",")).map(arreglo->{ //linea.split separa el string en las comas y hace un arreglo x linea
-				Jugador jugador = new Jugador (arreglo[2], arreglo[3], arreglo[4], arreglo[5], Integer.parseInt(arreglo[6]), arreglo[1], Byte.valueOf(arreglo[7]));
+			totJugadores = streamfile.map(linea -> linea.split (",")).map(arreglo->{ //linea.split separa el string en las comas y hace un arreglo x linea
+				Jugador jugador = new Jugador (arreglo[2], arreglo[3], (Date)arreglo[4], arreglo[5], Integer.parseInt(arreglo[6]), (Posicion)arreglo[1], Byte.valueOf(arreglo[7]));
 				return jugador;
 			}).collect(Collectors.toList());
 			
-			jugador.forEach(System.out::println);
+			totJugadores.forEach(System.out::println);
 		
 			
 		} catch (IOException io) {
 			System.err.println("Error en lectura de archivo" + io.getMessage());
 			
 		}
-		String filePath = "Equipos.txt";
-		List <Equipo> equipos = new ArrayList<>();
-		try (Stream <String> streamfile = Files.lines(Paths.get(filePath))){
-			equipos = streamfile.map(linea -> linea.split (",")).map(arreglo->{ 
-				Equipo equipo = new Equipo (arreglo[0], arreglo[1], Integer.parseInt(arreglo[2]), ArrayList<Jugador> j, Dt e);
-			return equipo;
-			}).collect(Collectors.toList());
+		//recorro totJugadores y voy creando una lista d jugadores x equipo
+		
+		for (int i=0;i<totJugadores.size();i++) { 
 			
-			equipos.forEach(System.out::println);
+			List <Jugador> jugadores = new ArrayList<>();
+			
+			for (int j=0; j<18; j++) { 
+		    
+				jugadores.add(totJugadores.get(j));
+		    	
+				//recorrer el equipos.txt y guardo en cada equipo su arraylist de jugadores
+		    	
+				String filePath = "Equipos.txt";
+		    	
+				List <Equipo> equipos = new ArrayList<>();
+		    	
+				try (Stream <String> streamfile = Files.lines(Paths.get(filePath))){
+		    		  equipos = streamfile.map(linea -> linea.split (",")).map(arreglo->{ 
+		    			  Dt dt = new Dt (arreglo[3], arreglo[4], (Date)arreglo[5], Integer.parseInt(arreglo[7]), arreglo[8], Byte.valueOf(arreglo[9]));
+		    			  Equipo equipo = new Equipo (arreglo[0], arreglo[1], Integer.parseInt(arreglo[2]),
+		    					  jugadores, dt);
+		    			  return equipo;
+		    		  }).collect(Collectors.toList());
+			
+		    		  equipos.forEach(System.out::println);
 		
 			
-		} catch (IOException io) {
-			System.err.println("Error en lectura de archivo" + io.getMessage());
+		    	  } catch (IOException io) {
+		    		  System.err.println("Error en lectura de archivo" + io.getMessage());
 			
-		}
+		    	  }
+		      }
+		      System.out.println(totJugadores.get(i));
+		    }
+		
+	
 	}
 }
 
