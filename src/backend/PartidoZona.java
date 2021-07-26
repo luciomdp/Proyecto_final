bccde6a1addcfba929ab3ed4adef26862f2e237d
matii <matii@DESKTOP-OGIGIUN>
2021-07-26 18:40:25 -0300

    se agrego el algoritmo que crea todas las fechas posibles a partir de un array de equipos (hay que ver de cambiar lo que devuelve!!!)

--- src/backend/PartidoZona.java
+++ src/backend/PartidoZona.java
@@ -1,5 +1,8 @@
 package backend;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class PartidoZona extends Partido{
 	private int GolE1, GolE2;
@@ -30,6 +33,58 @@ public class PartidoZona extends Partido{
 
     public void setGolE2(int golE2) {
         GolE2 = golE2;
+    }
+    
+    public static int factorial (int numero) {
+        if (numero == 0)
+            return 1;
+        else
+            return numero * factorial(numero - 1);
+    }
+
+    public static void creaFechas (String[] equipos) {
+
+        /*  Calculo primero todas las fórmulas que necesito y numeros que me sirven para hacer el algoritmo
+        partidosSimultaneos = cantEquipos/2;
+        partidosPosibles = (variacion entre cantEquipos y 2 (cantEquipos por partido)) / partidosSimultaneos;
+        cantFechas = partidosPosibles / partidosSimultaneos;
+        -----------------------------------------------------
+        partidosSimultaneos = 4/2 = 2;
+        partidosPosibles = ((4)!/(2)!) / 2 = (24/2) / 2 = 6;
+        cantFechas = 6 / 2 = 3;
+        */
+
+        ArrayList<String> fechas = new ArrayList<>();
+        int partidosSimultaneos = (equipos.length / 2);
+        int partidosPosibles = ((factorial(equipos.length))/2) / partidosSimultaneos;
+        int cantFechas = partidosPosibles / partidosSimultaneos;
+        int i = 0; //indice de fechas
+
+        //paso a minuscula
+        for (int k = 0; k < equipos.length; k++) {
+            equipos[k] = equipos[k].toLowerCase();
+        }
+
+        String[] equiposQueRotan = Arrays.copyOfRange(equipos, 1, equipos.length);
+        //mientras que no llegue a completar todas las fechas
+        while (i < cantFechas) {
+            String posibleFecha = equipos[0] + " "; //agrego el primero ya que queda fijo en todas las fechas;
+            //la primera fecha se agregan los equipos como están, es decir, 1vs2 3vs4 5vs6 7vs8 .....
+            if (i == 0) {
+                for (int k = 1; k < equipos.length; k++) {
+                    posibleFecha += equipos[k] + " ";
+                }
+            } else {
+                //aca viene el quilombo, tengo que ir rotando todos los equipos, 2 en 3-3 en 4-4 en 5-5 en 6...
+                Collections.rotate(Arrays.asList(equiposQueRotan), equiposQueRotan.length - 1);
+                //aca esta el array rotado asique ya puedo meter los equipos que faltan
+                for (int k = 0; k < equiposQueRotan.length; k++) {
+                    posibleFecha += equiposQueRotan[k] + " ";
+                }
+            }
+            fechas.add(posibleFecha);
+            i++;
+        }
     }
 
 }
