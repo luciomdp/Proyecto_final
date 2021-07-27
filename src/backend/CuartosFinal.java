6d942d6d9337019d19c12e4926ed65fd08a8c18b
gc <gc@192.168.0.155>
2021-07-27 18:45:43 -0300

    Arregle un par de cosas mas

--- src/backend/CuartosFinal.java
+++ src/backend/CuartosFinal.java
@@ -15,15 +15,19 @@ public class CuartosFinal {
 	public CuartosFinal(Equipo equipos []) {
 		this.equipos = equipos;
 		for (int i = 0; i < CANTE; i++) {
-			equipos[i].setGoles(0);
+			equipos[i].setGoles(0); //RESETEO LOS GOLES DE CADA EQUIPO A PARTIR DE ESTA INSTANCIA PARA PODER SABER LOS RESULTADOS DE LA IDA A COMPARACION DE LA VUELTA
 		}
 		this.partidos = new PartidoIdaVuelta [CANT_P] ;
 		int k= 0;
-		for (int i = 0; i < CANT_P; i++) {
-			for (int j = 1; i < CANT_P && i < j; i++) {
-				partidos[k].setEquipo1(equipos[i]);
-				partidos[k].setEquipo2(equipos[j]);
-			}
+		for (int i = 0; i < CANT_P/2; i+=2) {
+			partidos[k].setEquipo1(equipos[i]);
+			partidos[k].setEquipo2(equipos[i++]);
+			k++;
+		}
+		for (int i = 0; i < CANT_P/2; i+=2) {
+			partidos[k].setEquipo1(equipos[i++]);
+			partidos[k].setEquipo2(equipos[i]);
+			k++;
 		}
 		this.resultados = new Resultados [CANT_P];
 		this.i = 0;
@@ -32,6 +36,12 @@ public class CuartosFinal {
 	public void SimulaIda () {
 		if (i < CANT_P/2){
 			partidos[i].simulacionNM();
+			partidos[i].getEquipo1().setGoles(partidos[i].getGolesE1());
+			partidos[i].getEquipo2().setGoles(partidos[i].getGolesE2());
+			partidos[i].getEquipo1().setGolesContra(partidos[i].getGolesE2());
+			partidos[i].getEquipo2().setGolesContra(partidos[i].getGolesE1());
+			resultados[i].setE1(partidos[i].getEquipo1());
+			resultados[i].setE2(partidos[i].getEquipo2());
 			i++;
 		}
 		else
