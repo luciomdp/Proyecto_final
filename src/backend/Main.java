a693d70d67b86949ea8738632e1b5e8a66464e1f
Valen <gc@DESKTOP-BNEAP4S>
2021-08-28 20:43:10 -0300

    Empezamos a leer el xml con mati.

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -3,6 +3,15 @@ package backend;
 import java.io.*;
 import java.util.*;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import frontend.Frame;
 import proyecto_final.Controlador;
 
@@ -18,138 +27,111 @@ public class Main {
 	
 	public static void main (String[] args) throws NumberFormatException, ParseException {
 		
-		//-------------------------------------------------<<VARIABLES>>-------------------------------------------------
-		
-		String Jugadores = "Jugadores.txt"; String Equipos = "Equipos.txt"; String Referis = "Referis.txt";
-		BufferedReader br = null;
-		String line = ""; String coma = ",";
-		
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate fecha;
-				
-		Pais pais; Posicion pos;
-		
-		ArrayList <Jugador> totJugadores = new ArrayList<>();
-		((ArrayList<Jugador>) totJugadores).ensureCapacity(288);
-		                         
-		int j=0,  i= 0;
-		
-		//-------------------------------------------------<<JUGADORES>>-------------------------------------------------
 		
 		try {
-		    br = new BufferedReader(new FileReader(Jugadores));
-		    while ((line = br.readLine()) != null) {  
-		    	
-		        String[] datos = line.split(coma);  
-		        fecha = LocalDate.parse(datos[4], formato);
-		        pos = Posicion.valueOf(datos[1]);
-		        
-		        Jugador jugador = new Jugador (datos[2], datos[3], fecha, datos[5], Integer.parseInt(datos[6]), 
-		        								pos , Byte.parseByte(datos[7]));
-		        totJugadores.add(jugador);
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-		    if (br != null) {
-		        try {
-		            br.close();
-		        } catch (IOException e) {
-		            e.printStackTrace();
-		        }
-		    }
+			
+			File archivo = new File ("torneo.xml");
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			dbf.setValidating(true);
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(archivo);
+			doc.getDocumentElement().normalize();
+			
+			
+			System.out.println(doc.getDocumentElement().getNodeName());
+			Element raiz = doc.getDocumentElement();
+			leeArchivo(raiz);
+			
+			
+		}catch (Exception e) {
+			
+			e.printStackTrace();
+			
 		}
-		//totJugadores.forEach(System.out::println);
 		
-		//-------------------------------------------------<<EQUIPOS>>-------------------------------------------------
 		
-		equipos = new ArrayList<>(); 
-		(equipos).ensureCapacity(16);
-				
-				try {
-				    br = new BufferedReader(new FileReader(Equipos));
-				    line = br.readLine();
-			    	
-				    while (line!= null) {
-				    		jugadores = new ArrayList<>(); //array cada 18 jugadores
-				    		((ArrayList<Jugador>) jugadores).ensureCapacity(18);
-					    
-				    		for (j=0; j<18; j++) {
-				    			jugadores.add(totJugadores.get(i+j));
-				    		}
-				    		i=i+18;
-				    		
-				    		String[] datos = line.split(coma);
-				    		fecha = LocalDate.parse(datos[5], formato);
-				    		pais = Pais.valueOf(datos[8]);
-						
-				    		Dt dt = new Dt (datos[3], datos[4], fecha, Integer.parseInt(datos[7]), datos[6], pais, 
-				    						Byte.parseByte(datos[9]));
-				    		
-				    		pais = Pais.valueOf(datos[1]);
-				    		Equipo equipo = new Equipo(datos[0], pais, Integer.parseInt(datos[2]), jugadores, dt); 
-				    		equipos.add(equipo);
-				    		
-				    		line = br.readLine();				    	
-				   }
-				} catch (FileNotFoundException e) {
-				    e.printStackTrace();
-				} catch (IOException e) {
-				    e.printStackTrace();
-				} finally {
-				    if (br != null) {
-				        try {
-				            br.close();
-				        } catch (IOException e) {
-				            e.printStackTrace();
-				        }
-				    }
-				   System.out.println(("Equipos:\n"));
-					equipos.forEach(System.out::println);
-			      }
-		//-------------------------------------------------<<REFERIS>>-------------------------------------------------
-				
-		referis = new ArrayList <>();
-				try {
-				    br = new BufferedReader(new FileReader(Referis));
-				    line = br.readLine();
-			    	
-				    while (line!= null) {
-				    		
-				    	String[] datos = line.split(coma);
-			    		fecha = LocalDate.parse(datos[2], formato);
-			    		pais = Pais.valueOf(datos[5]);
-				
-			    		Referi referi = new Referi (datos[0], datos[1], fecha, Integer.parseInt(datos[4]), datos[3], pais, 
-		    										Byte.parseByte(datos[6]));
-			    		referis.add(referi);
-	
-			    		line = br.readLine();	
-			    		
-				   }
-				} catch (FileNotFoundException e) {
-				    e.printStackTrace();
-				} catch (IOException e) {
-				    e.printStackTrace();
-				} finally {
-				    if (br != null) {
-				        try {
-				            br.close();
-				        } catch (IOException e) {
-				            e.printStackTrace();
-				        }
-				    }
-				  /*  System.out.println(("Referis:\n"));
-				    referis.forEach(System.out::println);*/
-			      }
-				
 		Campeonato torneo = new Campeonato (equipos, jugadores, referis);
 		Frame vista = new Frame ();
 		Controlador control = new Controlador (torneo, vista);
 		vista.setControlador(control);
 		torneo.setControlador(control);
 		
-	}		
+	}	
+	public static void leeArchivo(Element _raiz) {
+
+        final NodeList children = _raiz.getChildNodes(); //aca sacamos equipos y arbitros
+        
+        for (int i=0; i<children.getLength(); i++) { //aca iteramos dos veces
+        	
+        	final Node nodo = children.item(i); //sacamos equipos o arbitros
+        	
+        	if (nodo.getNodeType() == Node.ELEMENT_NODE) { //nos fijamos si es un nodo
+
+        		if (nodo.getNodeName() == "equipos") { //agarramos equipos
+         			
+         			NodeList listaChildren = nodo.getChildNodes(); //lista de equipo's
+         			
+         			for (int j = 0; j<listaChildren.getLength(); j++) { 
+         				
+         				Node children2 = listaChildren.item(j); //agarramos un equipo
+         				
+         				if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+         					
+         					if (children2.getNodeName() == "equipo") { 
+         						
+         						System.out.println(children2.getNodeName());
+         						System.out.println(children2.getChildNodes().item(1).getTextContent());
+         						//1-nombre; 3-pais; 5-ranking
+         						
+         						NodeList listaChildren2 = children2.getChildNodes().item(7).getChildNodes(); 
+         						//entramos a plantel: jugadores y dt
+         						for (int k=0; k<listaChildren2.getLength(); k++) {
+         							
+         							Node children3 = listaChildren2.item(k); //jugador
+         							
+         							if (children3.getNodeName() == "jugador") {
+         								
+         								//instanciar jugador
+         								
+         							} else if (children3.getNodeName() == "dt") {
+         								
+         								
+         								
+         							}
+         							
+         						}
+         						
+         						
+         						
+         						
+         						
+         						
+         						
+         						
+         						//aca creamos el equipo con jugadores y dt
+         						//Equipo e = new Equipo();
+         						//ArrayList equipos.push
+         						
+         					}
+         				}
+                  				
+         			}
+         			
+         				
+         			//System.out.println(nodo.getNodeName() + ": " + (!nodo.hasChildNodes() ? "" : nodo.getTextContent()));
+         		
+         		
+         		} else if (nodo.getNodeName() == "arbitros") {
+         			
+         			
+         			
+         		}
+		 	}
+        	
+        }
+	}
 }
+
+
+
+
