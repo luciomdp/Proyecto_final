c2d8e161584889d82fa814135041b55753625bd1
gc <gc@192.168.0.107>
2021-10-12 13:16:41 -0300

    ultimo cambio

--- src/backend/Partido.java
+++ src/backend/Partido.java
@@ -46,10 +46,10 @@ public class Partido implements Serializable{
 		double probabilidadesGolB = 0;
 		int i;
 	
-		probabilidadesGolA = (Math.random()*100 *0.2) + ((equipo1.getMediaPos (Posicion.delantero) * 0.75 + equipo1.getMediaPos (Posicion.mediocampista) * 0.25)*0.6 *10) - ((equipo2.getMediaPos (Posicion.defensor) * 0.6 + equipo2.getMediaPos (Posicion.arquero) * 0.4)*0.4 *10);
+		probabilidadesGolA = (Math.random()*100 *0.2) + ((equipo1.getMediaPos (Posicion.delantero) * 0.75 + equipo1.getMediaPos (Posicion.mediocampista) * 0.25)*0.4 *10) - ((equipo2.getMediaPos (Posicion.defensor) * 0.6 + equipo2.getMediaPos (Posicion.arquero) * 0.4)*0.4 *10);
 		if (probabilidadesGolA < 10)
 			probabilidadesGolA = 5; // minimamente por partido el equipo tiene 5% probabilidades de que lo metan
-		probabilidadesGolA = (Math.random()*100 *0.2) + ((equipo2.getMediaPos (Posicion.delantero) * 0.75 + equipo2.getMediaPos (Posicion.mediocampista) * 0.25)*0.6 *10) - ((equipo1.getMediaPos (Posicion.defensor) * 0.6 + equipo1.getMediaPos (Posicion.arquero) * 0.4)*0.4 *10);
+		probabilidadesGolA = (Math.random()*100 *0.2) + ((equipo2.getMediaPos (Posicion.delantero) * 0.75 + equipo2.getMediaPos (Posicion.mediocampista) * 0.25)*0.4 *10) - ((equipo1.getMediaPos (Posicion.defensor) * 0.6 + equipo1.getMediaPos (Posicion.arquero) * 0.4)*0.4 *10);
 		if (probabilidadesGolB < 10)
 			probabilidadesGolB = 5;
 		
